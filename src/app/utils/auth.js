9971aa918f9c669e89e3e118f2a02839007c6f64
Sahil Kandari <kandarisahil13@gmail.com>
2023-10-29 02:07:34 +0530

    secret changed to site

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -13,7 +13,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_URL,
+  site: process.env.NEXTAUTH_URL,
 }
 
 export const getAuthSession = () => getServerSession(authOptions);
